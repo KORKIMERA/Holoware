b5140ada4f90b977ff384c25ff77a037bc84f863
bizmonger <scott.nimrod@bizmonger.net>
2015-02-19 20:57:57 -0500

    Designing ConfigureArchitecture view

---
+++ ArchitectureModule/UI/Views/ConfigureArchitectureView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ArchitectureModule.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class ConfigureArchitectureView : UserControl
+    {
+        public ConfigureArchitectureView()
+        {
+            InitializeComponent();
+        }
+    }
+}
